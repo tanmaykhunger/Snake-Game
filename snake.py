912b2608c1d8a20688b50a4fc3bbe01f895e7743
Tanmay Khunger <117931543+tanmaykhunger@users.noreply.github.com>
2023-03-30 11:18:31 +0530

    yay finished

--- snake.py
+++ snake.py
@@ -1,101 +1,163 @@
+#imports
 import turtle
-import random
 import time
+import random
 
-#creating screen
-screen = turtle.Screen()
-screen.title("SNAKE GAME")
-screen.setup(width=700, height=700)
-screen.tracer(0)
-screen.bgcolor('#1d1d1d')
-
-#creating border
-turtle.speed(5)
-turtle.pensize(4)
-turtle.penup()
-turtle.goto(-310, 250)
-turtle.pendown()
-turtle.color("red")
-turtle.forward(600)
-turtle.right(90)
-turtle.forward(600)
-turtle.right(90)
-turtle.forward(600)
-turtle.right(90)
-turtle.forward(600)
-turtle.right(90)
-turtle.penup()
-turtle.hideturtle()
-
-#score
-score = 0;
 delay = 0.1
 
-#snake
-snake = turtle.Turtle() 
-snake.speed()
-snake.shape("square")
-snake.color("green")
-snake.penup()
-snake.goto(0, 0)
-snake.direction = 'stop'
+#scores
+score = 0
+high_score = 0
 
-#food
-fruit = turtle.Turtle()
-fruit.speed(0)
-fruit.shape("square")
-fruit.color("white")
-fruit.penup()
-fruit.goto(30, 30)
+#set up screen
+wn = turtle.Screen()
+wn.title("Snake Game")
+wn.bgcolor('yellow')
+wn.setup(width=600, height=600)
+wn.tracer(0)
 
-old_fruit = []
+#snake head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("black")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
 
-#scoring
-scoring = turtle.Turtle()
-scoring.speed(0)
-scoring.color("white")
-scoring.penup()
-scoring.hideturtle()
-scoring.goto(0, 300)
-scoring.write("Score: ", align="center", font=("Courier", 24, "bold"))
+#snake food
+food= turtle.Turtle()
+food.speed(0)
+food.shape("square")
+food.color("red")
+food.penup()
+food.goto(0,100)
 
-#define how to move
-def snake_go_up():
-    if snake.direction != "down":
-        snake.direction = "up"
+segments = []
 
-def snake_go_down():
-    if snake.direction != "up":
-        snake.direction = "down"
+#scoreboards
+sc = turtle.Turtle()
+sc.speed(0)
+sc.shape("square")
+sc.color("black")
+sc.penup()
+sc.hideturtle()
+sc.goto(0,260)
+sc.write("score: 0  High score: 0", align = "center", font=("ds-digital", 24, "normal"))
 
-def snake_go_left():
-    if snake.direction != "right":
-        snake.direction = "left"
-
-def snake_go_right():
-    if snake.direction != "left":
-        snake.direction = "right"
-
+#Functions
+def go_up():
+    if head.direction != "down":
+        head.direction = "up"
+def go_down():
+    if head.direction != "up":
+        head.direction = "down"
+def go_left():
+    if head.direction != "right":
+        head.direction = "left"
+def go_right():
+    if head.direction != "left":
+        head.direction = "right"
 def move():
-    if snake.direction == "up":
-        y = snake.ycor()
-        snake.sety(y + 20)
-    if snake.direction == "down":
-        y = snake.ycor()
-        snake.sety(y - 20)
-    if snake.direction == "left":
-        x = snake.xcor()
-        snake.sety(x - 20)
-    if snake.direction == "right":
-        x = snake.xcor()
-        snake.sety(x + 20)
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y+20)
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y-20)
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x-20)
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x+20)
 
+#keyboard bindings
+wn.listen()
+wn.onkeypress(go_up, "Up")
+wn.onkeypress(go_down, "Down")
+wn.onkeypress(go_left, "Left")
+wn.onkeypress(go_right, "Right")
 
-# keyboard binding
-screen.listen()
-screen.onkeypress(snake_go_up, "Up")
-screen.onkeypress(snake_go_down, "Down")
-screen.onkeypress(snake_go_left, "Left")
-screen.onkeypress(snake_go_left, "Right")
+#MainLoop
+while True:
+    wn.update()
 
-#main loop
+    #check collision with border area
+    if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
+        time.sleep(1)
+        head.goto(0,0)
+        head.direction = "stop"
+
+        #hide the segments of body
+        for segment in segments:
+            segment.goto(1000,1000) #out of range
+        #clear the segments
+        segments.clear()
+
+        #reset score
+        score = 0
+
+        #reset delay
+        delay = 0.1
+
+        sc.clear()
+        sc.write("score: {}  High score: {}".format(score, high_score), align="center", font=("ds-digital", 24, "normal"))
+
+    #check collision with food
+    if head.distance(food) <20:
+        # move the food to random place
+        x = random.randint(-290,290)
+        y = random.randint(-290,290)
+        food.goto(x,y)
+
+        #add a new segment to the head
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("black")
+        new_segment.penup()
+        segments.append(new_segment)
+
+        #shorten the delay
+        delay -= 0.001
+        #increase the score
+        score += 10
+
+        if score > high_score:
+            high_score = score
+        sc.clear()
+        sc.write("score: {}  High score: {}".format(score,high_score), align="center", font=("ds-digital", 24, "normal")) 
+
+    #move the segments in reverse order
+    for index in range(len(segments)-1,0,-1):
+        x = segments[index-1].xcor()
+        y = segments[index-1].ycor()
+        segments[index].goto(x,y)
+    #move segment 0 to head
+    if len(segments)>0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x,y)
+
+    move()
+
+    #check for collision with body
+    for segment in segments:
+        if segment.distance(head)<20:
+            time.sleep(1)
+            head.goto(0,0)
+            head.direction = "stop"
+
+            #hide segments
+            for segment in segments:
+                segment.goto(1000,1000)
+            segments.clear()
+            score = 0
+            delay = 0.1
+
+            #update the score     
+            sc.clear()
+            sc.write("score: {}  High score: {}".format(score,high_score), align="center", font=("ds-digital", 24, "normal"))
+    time.sleep(delay)
+wn.mainloop()   
